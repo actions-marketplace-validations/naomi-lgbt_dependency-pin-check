d0ff660d3df731a5c92cd4d64adb4419e3926fce
Naomi Carrigan <nhcarrigan@gmail.com>
2023-03-18 18:40:08 -0700

    feat: logging, fix regex

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const { join } = require('path');
     const packageJson = require(path);
     const { devDependencies, dependencies } = packageJson;
 
+    console.log(`Validating the following development dependencies:`)
+    console.table(devDependencies);
+
     for (const [key, value] of Object.entries(devDependencies)) {
-        if (/"^/.test(value) || /"~/.test(value)) {
+        if (/\^/.test(value) || /~/.test(value)) {
             console.warn(`devDependencies ${key} version is not fixed`);
             failed = true;
         }
     }
+
+    console.log(`Validating the following dependencies:`)
+    console.table(dependencies);
 
     for (const [key, value] of Object.entries(dependencies)) {
         if (/"^/.test(value) || /"~/.test(value)) {
